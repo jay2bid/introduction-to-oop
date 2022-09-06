9fbbd12e01bec4f279d408b00424c107fd9088b7
jay2bid <jaybidokwu@gmail.com>
2022-09-06 02:09:43 +0100

    Final

--- Intro_to_OOP/Intro_to_OOP/Program.cs
+++ Intro_to_OOP/Intro_to_OOP/Program.cs
@@ -1,12 +1,13 @@
 ﻿using Intro_to_OOP;
 
-Pigeon pippa = new Pigeon("Pippa", "Grey");
+Pigeon pippa = new Pigeon("Pppa", "Grey");
 pippa.Speak();
 pippa.Fly();
 pippa.EatPizza();
 pippa.Spin();
 pippa.DoTheCaterpillar();
 pippa.Jump();
+
 Penguin pingu = new Penguin("Pingu", "Black & White");
 pingu.Speak();
 pingu.Fly();
